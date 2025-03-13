cd66c43e50554a161096ffb3d0a6c21d83a2f4bc
Alika1-cmd <alikacahyacarisa@mail.ugm.ac.id>
2025-03-13 13:39:24 +0700

    Update Latihan 4.py

--- Pertemuan 1/Latihan 4.py
+++ Pertemuan 1/Latihan 4.py
@@ -1,19 +1,19 @@
 class Tiket:
-    def __init__(self, kategori, harga_satuan):
+    def __init__(self, kategori, harga_jual):
         self.kategori = kategori
-        self.harga_satuan = harga_satuan 
+        self.harga_jual = harga_jual 
         
-    def __mul__(self, penjualan):
-        print('Jumlah tiket terjual:', penjualan.jumlah_tiket, 'tiket')
-        return self.harga_satuan * penjualan.jumlah_tiket
+    def __mul__(self, banyak_penjualan):
+        print('Jumlah tiket terjual:', banyak_penjualan.jumlah_tiket, 'tiket')
+        return self.harga_jual * banyak_penjualan.jumlah_tiket
 
 
-class PenjualanTiket:
+class Penjualan_Tiket:
     def __init__(self, kategori, jumlah_tiket):
         self.kategori = kategori
         self.jumlah_tiket = jumlah_tiket
 
-eksklusif = Tiket("Eksklusif", 9000)  
-penjualan_maret = PenjualanTiket("Eksklusif", 20) 
+eksklusif = Tiket("Eksklusif", 9000) 
+total_penjualan_mingguan = Penjualan_Tiket("Eksklusif", 20)  
 
-print(f"Total pendapatan dari kategori {eksklusif.kategori}: {eksklusif * penjualan_maret} ribu")
+print(f"Total pendapatan dari {eksklusif.kategori}: {eksklusif * total_penjualan_mingguan} ribu")
