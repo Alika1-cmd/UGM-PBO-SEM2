2f7fc241575c0fa6d0b6279409b6b534e554d779
Alika1-cmd <alikacahyacarisa@mail.ugm.ac.id>
2025-03-25 22:44:49 +0700

    Create Latihan 2.py

---
+++ Pertemuan 6/Latihan 2.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+class Person(ABC):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    @abstractmethod
+    def get_role(self):
+        pass
+
+#Subclass: Siswa
+class Student(Person):
+    def __init__(self, name, age, student_id):
+        super().__init__(name, age)
+        self.student_id = student_id
+    
+    def get_role(self):
+        return f"Siswa: {self.name}, Umur: {self.age}, ID Siswa: {self.student_id}"
+
+#Subclass: Guru
+class Teacher(Person):
+    def __init__(self, name, age, subject):
+        super().__init__(name, age)
+        self.subject = subject
+    
+    def get_role(self):
+        return f"Guru: {self.name}, Umur: {self.age}, Mata Pelajaran: {self.subject}"
+
+# Membuat objek siswa dan guru
+siswa1 = Student("Ahmad", 16, "S1234")
+guru1 = Teacher("Bu Rina", 35, "Matematika")
+
+# Menampilkan informasi peran 
+print(siswa1.get_role())
+print(guru1.get_role())
