c8532328790f9a190c225c4faf3f6a3ebae8c15d
Alika1-cmd <alikacahyacarisa@mail.ugm.ac.id>
2025-05-08 12:55:06 +0700

    Update code 2.py

--- Pertemuan 11/code 2.py
+++ Pertemuan 11/code 2.py
@@ -5,7 +5,7 @@ class Model(object):
         'voice': {'number': 1000, 'price': 15},
     }
 
-class View(object):
+class View(object):  # English
     def list_services(self, services):
         print("Services Provided:")
         for svc in services:
@@ -18,7 +18,7 @@ class View(object):
                   svc, "message you pay $",
                   Model.services[svc]['price'])
 
-class View2(object):  # Bahasa Indonesia
+class View2(object):  # Indonesian
     def list_services(self, services):
         print("Layanan yang disediakan:")
         for svc in services:
@@ -32,21 +32,33 @@ class View2(object):  # Bahasa Indonesia
                   Model.services[svc]['price'])
 
 class Controller(object):
-    def __init__(self, language_choice):
+    def __init__(self, view):
         self.model = Model()
-        if language_choice == '1':
-            self.view = View()
-        elif language_choice == '2':
-            self.view = View2()
-        else:
-            self.view = None
+        self.view = view
 
     def get_services(self):
         services = self.model.services.keys()
-        if self.view:
-            self.view.list_services(services)
+        return self.view.list_services(services)
 
     def get_pricing(self):
         services = self.model.services.keys()
-        if self.view:
-            self.view.list_pricing(services)
+        return self.view.list_pricing(services)
+
+# Mapping bahasa ke view class
+language_map = {
+    '1': View(),
+    '2': View2()
+}
+
+# Input dari user
+lang = input("What language do you choose? [1]English [2]Indonesia: ")
+view = language_map.get(lang)
+
+# Tampilkan hasil jika valid, jika tidak error
+if view:
+    controller = Controller(view)
+    controller.get_services()
+    controller.get_pricing()
+else:
+    print("Error, choose the language number!")
+    print("Error, choose the language number!")
